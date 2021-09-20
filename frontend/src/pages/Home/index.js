225d946cf1ad1bcb1141c2d1783403116a51e72b
André Carvalho <andre_carvalho0@live.com>
2021-09-20 19:48:25 -0300

    finalling the home

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import Emphasis from '../../components/Emphasis'
 import NavBar from '../../components/NavBar'
 import './styles.css'
+import Image from '../../assets/emphasis.png'
 
 export default class Home extends Component {
 
@@ -10,8 +10,36 @@ export default class Home extends Component {
             <body>
                 <NavBar />
                 <main>
-                    
-                    <Emphasis />
+
+                    <section id="home">
+                        <div>
+                            <h1 className="emphasis_title">Onde há <br />VONTATE,</h1>
+                            <h1 className="emphasis_hover_title">há sempre <br />um AMARELINHO</h1>
+                        </div>
+                        <div>
+                            <img src={Image} alt="Imagem_Ilustrativa" className="image_emphasis" />
+                            <div className="circle" />
+                        </div>
+
+                    </section>
+
+                    <section id="delivery">
+                        <h1>Delivery</h1>
+                        <h2>Peça pelo nosso...</h2>
+
+                        <div className="delivery_itens">
+                            <div>
+                                <i class='bx bxl-android delivery_icon' />
+                                <i class='bx bxl-apple delivery_icon' />
+                                <h3>Aplicativo</h3>
+                            </div>
+                            <div>
+                                <i class='bx bxs-pointer delivery_icon' />
+                                <h3>Site</h3>
+                            </div>
+                        </div>
+                    </section>
+
                 </main>
             </body>
         )
